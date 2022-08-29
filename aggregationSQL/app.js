276928381c07f8c159368ea080f50cd04bd1a60d
InterLink Intern <intern@intern-pc.interlink.office>
2022-08-29 12:40:31 +0300

    add corse in pcg.json

--- aggregationSQL/app.js
+++ aggregationSQL/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const router = require("./routes");
 
 function logRequest({ method, url }, res, next) {
@@ -8,6 +9,7 @@ function logRequest({ method, url }, res, next) {
 }
 
 app.use(logRequest);
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
